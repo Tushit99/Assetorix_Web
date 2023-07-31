2875e4cae2bd95b9398697c2bca3f701fefde8ee
Tushit99 <97578587+Tushit99@users.noreply.github.com>
2023-07-31 10:27:36 +0530

    home_page

--- src/components/Navbar/Navebar.jsx
+++ src/components/Navbar/Navebar.jsx
@@ -3,8 +3,25 @@ import Logo from "../Logo/Logo";
 import style from "./Navbar.module.css";
 import { Link } from "react-router-dom";
 import Hamburger from "../Hamburger/Hamburger";
-import { Popover, PopoverArrow, PopoverBody, PopoverContent, PopoverHeader, PopoverTrigger, Button, Box, Text } from "@chakra-ui/react";
-import { BiSolidUserDetail } from "react-icons/bi"
+import {
+    Popover,
+    PopoverArrow,
+    PopoverBody,
+    PopoverContent,
+    PopoverHeader,
+    PopoverTrigger,
+    Button,
+    Box,
+    Text,
+    Tabs,
+    Tab,
+    TabPanel,
+    TabList,
+    TabPanels,
+    Checkbox,
+    PopoverCloseButton,
+} from "@chakra-ui/react";
+import { BiSolidUserDetail } from "react-icons/bi";
 
 const Navebar = () => {
     return (
@@ -17,7 +34,7 @@ const Navebar = () => {
                     <Logo />
                 </div>
                 <div className={style.login_data}>
-                    <div className={style.country} >
+                    <div className={style.country}>
                         <select style={{ border: "0px", outline: "0px" }}>
                             <option value="india">India</option>
                             <option value="usa">USA</option>
@@ -38,11 +55,22 @@ const Navebar = () => {
                         <PopoverContent>
                             <PopoverArrow />
                             <PopoverHeader>Welcome to Assetorix</PopoverHeader>
-                            <PopoverBody > 
-                                <Text margin={"0 0 8px 0"}>Login for more futuristic experience</Text>
-                                <Box display={"flex"} justifyContent={"space-around"} margin={"0 0 8px 0"} alignItems={"center"}>
-                                    <Link className={style.logbtn} to={"/login"}>Login</Link>
-                                    <Link className={style.logbtn} to={"/signup"}>Sign Up</Link>
+                            <PopoverBody>
+                                <Text margin={"0 0 8px 0"}>
+                                    Login for more futuristic experience
+                                </Text>
+                                <Box
+                                    display={"flex"}
+                                    justifyContent={"space-around"}
+                                    margin={"0 0 8px 0"}
+                                    alignItems={"center"}
+                                >
+                                    <Link className={style.logbtn} to={"/login"}>
+                                        Login
+                                    </Link>
+                                    <Link className={style.logbtn} to={"/signup"}>
+                                        Sign Up
+                                    </Link>
                                 </Box>
                             </PopoverBody>
                         </PopoverContent>
@@ -50,27 +78,103 @@ const Navebar = () => {
                 </div>
             </div>
             <div className={style.nav_bottom}>
-                <Link to={"/buy"}>Buy</Link>
-                <Link >Sell</Link>
+                {/* Buy button  */}
+                <Popover>
+                    <PopoverTrigger>
+                        <Button
+                            backgroundColor={"unset"}
+                            color={"black"}
+                            fontWeight={300}
+                            _hover={{ color: "unset" }}
+                            _active={{ color: "unset" }}
+                        >
+                            Buy
+                        </Button>
+                    </PopoverTrigger>
+                    <PopoverContent w={{ base: "320px", md: "400px" }}>
+                        <PopoverArrow /> 
+                        <PopoverHeader>Buy Property</PopoverHeader>
+                        <PopoverBody>
+                            {/* one */}
+                            <Tabs variant="enclosed">
+                                <TabList>
+                                    <Tab>Residential</Tab>
+                                    <Tab>Commercial</Tab>
+                                </TabList>
+                                <TabPanels>
+                                    <TabPanel>
+                                        <Box className={style.buy}>
+                                            <Checkbox iconSize={"0px"} colorScheme={"blue"}>
+                                                Flat/Apartment
+                                            </Checkbox>
+                                            <Checkbox iconSize={"0px"} colorScheme={"blue"}>
+                                                Independent House/villa
+                                            </Checkbox>
+                                            <Checkbox iconSize={"0px"} colorScheme={"blue"}>
+                                                Independent/builder Floor
+                                            </Checkbox>
+                                            <Checkbox iconSize={"0px"} colorScheme={"blue"}>
+                                                Serviced Apartment
+                                            </Checkbox>
+                                            <Checkbox iconSize={"0px"} colorScheme={"blue"}>
+                                                Plot/Land
+                                            </Checkbox>
+                                            <Checkbox iconSize={"0px"} colorScheme={"blue"}>
+                                                1 RK/ Studio Apartment
+                                            </Checkbox>
+                                            <Checkbox iconSize={"0px"} colorScheme={"blue"}>
+                                                Farmhouse
+                                            </Checkbox>
+                                            <Checkbox iconSize={"0px"} colorScheme={"blue"}>
+                                                other
+                                            </Checkbox>
+                                        </Box>
+                                        <Button backgroundColor={"rgb(46,49,146)"} color={"white"} className={style.start_btn}>Start Now</Button>
+                                    </TabPanel>
+                                    <TabPanel>
+                                        <Box className={style.buy}>
+                                            <Checkbox size={"md"} color={"black"}>
+                                                Office
+                                            </Checkbox>
+                                            <Checkbox>Retail</Checkbox>
+                                            <Checkbox>Plot/Land</Checkbox>
+                                            <Checkbox>Storage</Checkbox>
+                                            <Checkbox>Industry</Checkbox>
+                                            <Checkbox>Hospitality</Checkbox>
+                                            <Checkbox>Other</Checkbox>
+                                        </Box>
+                                        <Button backgroundColor={"rgb(46,49,146)"} color={"white"} className={style.start_btn}>Start Now</Button>
+                                    </TabPanel>
+                                </TabPanels>
+                            </Tabs>
+                        </PopoverBody>
+                    </PopoverContent>
+                </Popover>
+                {/* ghbnjkl */}
+                <Link>Sell</Link>
                 <Link>Home Loans</Link>
                 <Link>Rent</Link>
                 <Link>Advertise</Link>
                 <Link>Agent Finder</Link>
-                <Popover >
+                <Popover>
                     <PopoverTrigger>
                         <button>Corporate Services</button>
                     </PopoverTrigger>
                     <PopoverContent>
                         <PopoverArrow />
-                        <PopoverBody className={style.option} >
-                            <Link to={"/Construction_Management"}>Construction Management</Link>
+                        <PopoverBody className={style.option}>
+                            <Link to={"/Construction_Management"}>
+                                Construction Management
+                            </Link>
                             <Link to={"/partner"}>Partner with Us</Link>
                             <Link to={"/Property_Marketing"}>Property Marketing</Link>
-                            <Link to={"/acquisitions_and_dispositions"}>Acquisitions & Dispositions</Link>
+                            <Link to={"/acquisitions_and_dispositions"}>
+                                Acquisitions & Dispositions
+                            </Link>
                             {/* <Link to={"/consulting"}>Consulting</Link> */}
                             <Link to={"/marketresearch"}>Market Research</Link>
                             <Link to={"/portfolio_planning"}>Property & Portfolio Sales</Link>
-                            <Link to={"/usa_real_state"} > USA Real State</Link>
+                            <Link to={"/usa_real_state"}> USA Real State</Link>
                         </PopoverBody>
                     </PopoverContent>
                 </Popover>
