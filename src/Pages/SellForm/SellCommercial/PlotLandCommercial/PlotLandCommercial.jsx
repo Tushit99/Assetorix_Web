0a665128baca9876a8ba09d8b6d5359ae3d49361
Tushit99 <97578587+Tushit99@users.noreply.github.com>
2023-09-02 17:28:55 +0530

    data

--- src/Pages/SellForm/SellCommercial/PlotLandCommercial/PlotLandCommercial.jsx
+++ src/Pages/SellForm/SellCommercial/PlotLandCommercial/PlotLandCommercial.jsx
@@ -3,13 +3,14 @@ import style from "./PlotLandCommercial.module.css";
 import React, { useState } from 'react'
 import CommercialLand from './CommercialLand/CommercialLand';
 import AgricalturalFarm from './AgricalturalFarm/AgricalturalFarm';
+import IndustrialLand from './IndustrialLand/IndustrialLand';
 
 const PlotLandCommercial = () => {
     const [plotType, setPlotType] = useState("");
 
     return (
         <div>
-            <Box>
+            <Box className={style.optional_box}> 
                 <Heading size={"md"}>
                     What kind of plot / land is it?
                 </Heading>
@@ -17,7 +18,7 @@ const PlotLandCommercial = () => {
                     <button value={"Commercial Land / lnst. Land"} className={plotType == "Commercial Land / lnst. Land" ? style.setbtn : style.btn} onClick={(e) => setPlotType(e.target.value)} >
                         Commercial Land/lnst. Land
                     </button>
-                    <button value={"Agricultural x/ Farm Land"} className={plotType == "Agricultural / Farm Land" ? style.setbtn : style.btn} onClick={(e) => setPlotType(e.target.value)} >
+                    <button value={"Agricultural / Farm Land"} className={plotType == "Agricultural / Farm Land" ? style.setbtn : style.btn} onClick={(e) => setPlotType(e.target.value)} >
                         Agricultural/Farm Land
                     </button>
                     <button value={"Industrial Lands / Plots"} className={plotType == "Industrial Lands / Plots" ? style.setbtn : style.btn} onClick={(e) => setPlotType(e.target.value)} >
@@ -27,9 +28,9 @@ const PlotLandCommercial = () => {
 
                 {plotType == "Commercial Land / lnst. Land" && <CommercialLand />}
 
-                {plotType == "Agricultural x/ Farm Land" && <AgricalturalFarm />}  
+                {plotType == "Agricultural / Farm Land" && <AgricalturalFarm />}  
                  
-                
+                {plotType == "Industrial Lands / Plots" && <IndustrialLand />}                  
 
             </Box>
         </div>
