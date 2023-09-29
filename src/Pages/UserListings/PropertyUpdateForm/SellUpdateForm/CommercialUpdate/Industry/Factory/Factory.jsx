d768edfc8397f773a2852aeb8467d384b68a0836
Tushit99 <97578587+Tushit99@users.noreply.github.com>
2023-09-29 17:36:57 +0530

    data

--- src/Pages/UserListings/PropertyUpdateForm/SellUpdateForm/CommercialUpdate/Industry/Factory/Factory.jsx
+++ src/Pages/UserListings/PropertyUpdateForm/SellUpdateForm/CommercialUpdate/Industry/Factory/Factory.jsx
@@ -416,7 +416,7 @@ const FactoryUpdate = () => {
 
 
     return ( 
-        <div>
+        <Box w={"94%"} padding={"0 20px"} margin={"auto"} boxShadow={"rgba(100, 100, 111, 0.2) 0px 7px 29px 0px"} >
             <form onSubmit={handleSubmitData}>
                 <Box className={style.location_form}>
                     <Heading size={"lg"}>Where is your property located?</Heading>
@@ -1501,7 +1501,7 @@ const FactoryUpdate = () => {
                 </Button>
 
             </form>
-        </div>
+        </Box>
     )
 }
 
