16957a3e17b5ddb7ca8ac0170bf93bb2beb48315
Tushit99 <97578587+Tushit99@users.noreply.github.com>
2023-11-28 16:18:22 +0530

    data

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import style from "./Logo.module.css";
-import { Link } from 'react-router-dom';  
-import assetoix from "./assetorix.png"; 
+import { Link } from 'react-router-dom';
+import assetoix from "./assetorix.png";
 import { Image } from '@chakra-ui/react';
 
 const Logo = () => {
   return (
     <h2 className={style.logo}>
       <Link to={"/"}>
-        <Image src={assetoix} alt="logo" />    
+        <Image src={assetoix} alt="logo" /> 
       </Link> 
     </h2> 
-  ) 
+  )
 }
 
-export default Logo;  
+export default Logo; 
 
